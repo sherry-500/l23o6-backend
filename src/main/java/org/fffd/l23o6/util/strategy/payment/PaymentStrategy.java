5869a4aada0faa2475112cade4ef3d0b81a22687
sherry-500 <1845351383@qq.com>
2023-07-08 00:16:40 +0800

    支付策略接口完善

--- src/main/java/org/fffd/l23o6/util/strategy/payment/PaymentStrategy.java
+++ src/main/java/org/fffd/l23o6/util/strategy/payment/PaymentStrategy.java
@@ -2,7 +2,7 @@ package org.fffd.l23o6.util.strategy.payment;
 
 public abstract class PaymentStrategy {
 
-    // : implement this by adding necessary methods and implement specified strategy
+    //implement this by adding necessary methods and implement specified strategy
     public String pay(Long orderId, String seat, double amount){
         //pay the order
         return null;
